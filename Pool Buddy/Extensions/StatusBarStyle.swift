ff4f3e4d930937001240f04a162d76d39a573da6
Allen Boynton <alboynton4@gmail.com>
2017-11-19 20:42:28 -0500

    Added status bar class
    Created right content status bar

---
+++ Pool Buddy/Extensions/StatusBarStyle.swift
@@ -0,0 +1,15 @@
+//
+//  StatusBarStyle.swift
+//  Pool Buddy
+//
+//  Created by Allen Boynton on 11/19/17.
+//  Copyright © 2017 Allen Boynton. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationController {
+    override open var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
